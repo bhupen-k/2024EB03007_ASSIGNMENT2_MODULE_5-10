e24e99db07b79146f7745c94b3a7cc48913924ee
bhupen-k <77371193+bhupen-k@users.noreply.github.com>
2026-02-05 23:46:38 +0530

    Add files via upload

---
+++ Question 10/signal_handling.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void handle_sigterm(int sig) {
+    printf("\nParent received SIGTERM signal. Performing cleanup...\n");
+}
+void handle_sigint(int sig) {
+    printf("\nParent received SIGINT signal. Exiting gracefully...\n");
+    exit(0);
+}
+int main() {
+    pid_t pid1, pid2;
+    printf("Parent process PID: %d\n", getpid());
+    signal(SIGTERM, handle_sigterm);
+    signal(SIGINT, handle_sigint);
+
+    pid1 = fork();
+    if (pid1 == 0) {
+        sleep(5);
+        kill(getppid(), SIGTERM);
+        exit(0);
+    }
+    pid2 = fork();
+    if (pid2 == 0) {
+        sleep(10);
+        kill(getppid(), SIGINT);
+        exit(0);
+    }
+    while (1) {
+        pause();
+    }
+    return 0;
+}
